4ddfbd47c5bd3c52fe4988a68e486caba5182f7d
JoshKarpel <karpel@wisc.edu>
2019-04-17 13:21:33 -0500

    testing no intermediate step...

--- binder/local.sh
+++ binder/local.sh
@@ -4,5 +4,5 @@ set -e
 
 CONTAINER_TAG=htcondor-python-bindings-tutorials
 
-docker build -t ${CONTAINER_TAG} --file docker/Dockerfile .
+docker build -t ${CONTAINER_TAG} --file binder/Dockerfile .
 docker run -it --rm -p 8888:8888 ${CONTAINER_TAG} jupyter lab
