e3c66a4e489638eeef7302026c5e2fc8149fe807
JoshKarpel <karpel@wisc.edu>
2019-04-18 10:54:02 -0500

    add a CMD for the image, change local and dev scripts accordingly

--- binder/dev.sh
+++ binder/dev.sh
@@ -5,4 +5,4 @@ set -e
 CONTAINER_TAG=htcondor-python-bindings-tutorials
 
 docker build -t ${CONTAINER_TAG} --file binder/Dockerfile .
-docker run -it --rm -p 8888:8888 --mount type=bind,source="$(pwd)"/tutorials,target=/home/jovyan/tutorials ${CONTAINER_TAG} jupyter lab
+docker run -it --rm -p 8888:8888 --mount type=bind,source="$(pwd)"/tutorials,target=/home/jovyan/tutorials ${CONTAINER_TAG}
