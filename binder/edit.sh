c4b15baa495b31a8621078ccf7a58d3fd7bed4fc
Josh Karpel <karpel@wisc.edu>
2020-02-25 09:13:25 -0600

    allow passing arguments through the edit and run scripts

--- binder/edit.sh
+++ binder/edit.sh
@@ -7,4 +7,4 @@ CONTAINER_TAG=htmap-binder-edit
 echo "Building HTCondor Tutorials Binder container..."
 
 docker build -t ${CONTAINER_TAG} --file binder/Dockerfile .
-docker run -it --rm -p 8888:8888 --mount type=bind,source="$(pwd)"/tutorials,target=/home/jovyan/tutorials ${CONTAINER_TAG}
+docker run -it --rm -p 8888:8888 --mount type=bind,source="$(pwd)"/tutorials,target=/home/jovyan/tutorials ${CONTAINER_TAG} $@
--- binder/run.sh
+++ binder/run.sh
@@ -7,4 +7,4 @@ CONTAINER_TAG=htmap-binder-run
 echo "Building HTCondor Tutorials Binder container..."
 
 docker build -t ${CONTAINER_TAG} --file binder/Dockerfile .
-docker run -it --rm -p 8888:8888 ${CONTAINER_TAG}
+docker run -it --rm -p 8888:8888 ${CONTAINER_TAG} $@
