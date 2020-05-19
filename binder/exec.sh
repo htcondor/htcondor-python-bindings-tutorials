88ad6b3290f426665c01a3466e07649c6c5c175d
Josh Karpel <josh.karpel@gmail.com>
2020-05-18 21:48:53 -0500

    manually clean up between tutorial renders in exec.sh

--- binder/exec.sh
+++ binder/exec.sh
@@ -7,4 +7,4 @@ CONTAINER_TAG=htcondor-python-bindings-tutorials
 echo "Building HTCondor Python Bindings Tutorials container..."
 
 docker build -t ${CONTAINER_TAG} --file binder/Dockerfile .
-docker run -it --rm --mount type=bind,source="$(pwd)"/tutorials,target=/home/jovyan/tutorials ${CONTAINER_TAG} -- bash -l -c "find_notebooks | xargs jupyter nbconvert --to notebook --inplace --execute --allow-errors"
+docker run --rm --mount type=bind,source="$(pwd)"/tutorials,target=/home/jovyan/tutorials ${CONTAINER_TAG} -- bash -l -c 'sleep 5 && condor_who -wait:60 "IsReady && STARTD_State =?= \"Ready\"" && for x in $(find_notebooks); do jupyter nbconvert --to notebook --inplace --execute --allow-errors --ExecutePreprocessor.timeout=None $x && condor_rm --all ; done'
